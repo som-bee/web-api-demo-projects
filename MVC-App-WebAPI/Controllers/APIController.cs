be5c35e7dc1eb78cdd73d1d8abdaa198e3cc0691
Somnath Bera <somnathbera1919@gmail.com>
2023-06-12 11:55:58 +0530

    Project 1 added

---
+++ MVC-App-WebAPI/Controllers/APIController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+using System.Web.Routing;
+
+namespace MVC_App_WebAPI.Controllers
+{
+
+    
+    public class APIController : ApiController
+    {
+        static int i = 1;
+        [Route("api/get/{id}")]
+        public IHttpActionResult Get([FromUri]int id) //mentioning that will be getting attribute from url only
+        {
+            i++;
+            return Ok(i);
+        }
+    }
+}
---
+++ MVC-App-WebAPI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVC_App_WebAPI.MvcApplication" Language="C#" %>
