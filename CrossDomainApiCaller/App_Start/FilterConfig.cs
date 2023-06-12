6e4965b923f63d5aa037008446cdf983d2dfc30b
Somnath Bera <somnathbera1919@gmail.com>
2023-06-12 12:03:19 +0530

    Project 4 added

---
+++ CrossDomainApiCaller/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace CrossDomainApiCaller
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
